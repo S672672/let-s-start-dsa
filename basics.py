944082beb39cd5f3040588e21a86d96c281b618f
Smith <bsmith672@rku.ac.in>
2024-01-03 21:56:16 +0530

    new commit

---
+++ basics.py
@@ -0,0 +1,18 @@
+#simple print statement in python
+# print("Hello smith");
+
+# #take user input in python
+# name = print("what is your name?");
+# print("Hello "+ name);
+
+#take multiple inputs in a single line
+# age,address = input("what is your age and where do you live?").split();
+# print(age)
+# print(address)
+
+#if else statement in python
+name = input("Enter your name\n");
+if(name == "smith"):
+    print("hey king welcome");
+else:
+    print("bhag bsdk");
