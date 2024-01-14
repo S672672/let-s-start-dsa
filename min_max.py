f353434bab5e6bd48994dd127e55402e1590eed4
Smith <bsmith672@rku.ac.in>
2024-01-14 13:13:09 +0530

    exercise of list completed

---
+++ min_max.py
@@ -0,0 +1,19 @@
+# numbers = [6,60,3];
+# print(min(numbers))
+# print(max(numbers))
+
+# def greatest(lists):
+#     diff = max(lists) - min(lists);
+#     return diff;
+# numbers = [70,80,90];
+# print(greatest(numbers))
+
+#exercise
+# def inside_lists(lists):
+#     count = 0;
+#     for i in lists:
+#         if type(i) == list:
+#             count += 1;
+#     return count;
+# numbers = [1,2,3,[4,5]];
+# print(inside_lists(numbers))
