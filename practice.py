e45b068c41c70e3ccaeaeaa36b5d1b7f8d9eda52
Smith <bsmith672@rku.ac.in>
2024-01-17 20:55:58 +0530

    practice

--- practice.py
+++ practice.py
@@ -1,9 +1,23 @@
 #fibonacci series
-def fibonacci_recursive(n):
-    if n <= 1:
-        return n
+# def fibonacci_recursive(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+# n_terms = 10
+# for i in range(n_terms):
+#     print(fibonacci_recursive(i))
+
+#sum of number from 1 to n
+def sum_of_numbers_recursive(n):
+    if n == 0:
+        return 0
     else:
-        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
-n_terms = 10
-for i in range(n_terms):
-    print(fibonacci_recursive(i))
+        return n + sum_of_numbers_recursive(n-1)
+
+n = 10
+result = sum_of_numbers_recursive(n)
+print(f"The sum of numbers from 1 to {n} is: {result}")
+
+
+
