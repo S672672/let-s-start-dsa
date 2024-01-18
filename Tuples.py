0940e80675e18904706da7b26d08dd69e68cb0c1
Smith <bsmith672@rku.ac.in>
2024-01-18 21:52:55 +0530

    started tuple

---
+++ Tuples.py
@@ -0,0 +1,38 @@
+#tuple is a data structure that can store any data type like list 
+#tuples are immutable
+#no append,no insert,no pop,no remove
+
+#methods in tuples
+#count
+#index
+#len
+#slicing
+
+# numbers = (1,2,3,4,5)
+# print(numbers[:2])
+
+#more about tuples
+#looping in tuples
+
+# mixed = (1,2,3,4,5);
+# for i in mixed:
+#     print(i)
+
+#create tuple with one element(use comma after the element)
+# num = (1,)
+# print(type (num))
+
+#tuple unpacking
+# name = ("smith","Arpan","Narayan","samjhana");
+# name1,name2,name3,name4 = name
+# print(name1)
+
+#list inside tuplee(we can use any list method in the list of the tuple)
+# name = ("smith","Arpan",["Narayan","samjhana"]);
+# print(name[2])
+
+#min,max,sum
+# mixed = (1,2,3,4,5);
+# print(min(mixed))
+# print(max(mixed))
+# print(sum(mixed))
