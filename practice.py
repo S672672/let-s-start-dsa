9332444d7e271fdb5604a228acdecd81e7aad62b
Smith <bsmith672@rku.ac.in>
2024-01-18 20:38:29 +0530

    factorial

--- practice.py
+++ practice.py
@@ -9,15 +9,24 @@
 #     print(fibonacci_recursive(i))
 
 #sum of number from 1 to n
-def sum_of_numbers_recursive(n):
-    if n == 0:
-        return 0
-    else:
-        return n + sum_of_numbers_recursive(n-1)
+# def sum_of_numbers_recursive(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + sum_of_numbers_recursive(n-1)
 
-n = 10
-result = sum_of_numbers_recursive(n)
-print(f"The sum of numbers from 1 to {n} is: {result}")
+# n = 10
+# result = sum_of_numbers_recursive(n)
+# print(f"The sum of numbers from 1 to {n} is: {result}")
+
+#factorial
+# def fact(num):
+#     if num == 1:
+#         return 1;
+#     else:
+#         return fact(num-1) * num
+# number = 5;
+# print(fact(number));
 
 
 
