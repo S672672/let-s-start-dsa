8e9d75e7538db1a51899e805df9bd2094e4ad798
Smith <bsmith672@rku.ac.in>
2024-01-19 11:10:05 +0530

    set practice

---
+++ set.py
@@ -0,0 +1,9 @@
+# set1 = {"smith","bhattarai",22};
+# print(set1)
+# set1.add('btech');#]add element in the set
+# print(set1);
+
+#adding two set
+name1 = {"smith","Bhattarai"};
+info = {"btech",22};
+print(name1.union(info));
